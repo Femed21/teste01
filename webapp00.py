bd694c4dfd3a4114ff166d87275708db69e27d76
Femed21 <felipemedina21@outlook.com>
2024-10-17 21:07:14 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,14 +1,9 @@
 # MEU PRIMEIRO WEB APP
 import streamlit as st
 from ACTlib01 import *
-
-Configurar_Pagina("Exemplo 1", 
-                    "amplo", 
-                    "auto", 
-                    "https://docs.streamlit.io", 
-                    "mailto:informacoes.actsp@gmail.com", 
-                    "ACT - Soluções para Pessoas. Você pode usar formatação Markdown para adicionar informações neste espaço. Para mais informações acesse *https://www.markdownguide.org*",
-                    "©️")
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSsvk88olrBp0YqM-jSjZ6nNx1xgWjRskSwwGp1F9fcioV8ORwWu2nik3ZjAG5J_xhmEq-Drwh6qUfC/pub?gid=466975264&single=true&output=csv"
+db = Ler_GooglePlanilha(url)
+Escrever(db)
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("TESTE 11")
@@ -22,15 +17,4 @@ st.subheader("TESTE FAKE")
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("É tipo o teste 10, mas é fake")
 
-Divisor()
-    
-coluna1 = Colunas(3)
-with coluna1[0]:
-  Escrever("")
-with coluna1[1]:
-  Escrever("ACTlib Versão 0.1", "titulo")
-  nome = Ler(rotulo = "Nome:", nmax=30, tipo="padrao", info="Inserção de Nome", autocompletar=None, na_mudanca=None, args=None, kwargs=None, placeholder="Não esqueça de preencher seu nome", desabilitada="falso", visibilidade="visivel")
-  if nome:     
-    Escrever("Seja Bem vinda(o), " + nome + "!")
-with coluna1[2]:
-  Escrever("")
+
